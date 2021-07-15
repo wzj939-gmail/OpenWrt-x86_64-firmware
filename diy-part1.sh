fe4851785128dd69cd9da23b9a97b96047e44b2f
wzj939-gmail <71462869+wzj939-gmail@users.noreply.github.com>
2021-07-16 00:14:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,3 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/uci-app-poweroff
